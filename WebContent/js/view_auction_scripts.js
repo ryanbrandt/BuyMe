0eeb2322b02ed4c5198ea78ae6325b2c87c0ca55
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2019-04-14 19:33:06 -0400

    start auto bidding

--- WebContent/js/view_auction_scripts.js
+++ WebContent/js/view_auction_scripts.js
@@ -21,7 +21,7 @@ $('#bidForm').on('submit', function(e){
 			amount.attr('placeholder', 'Min: $' + (parseFloat(bid)+0.01));
 			amount.attr('min', (parseFloat(bid)+0.01));
 			document.getElementById('load').style.display = "none";
-			$('#close').click();
+			$('#bidClose').click();
 			alert('Success! You Bid $' + bid);
 		}
 	})
@@ -37,7 +37,7 @@ $('#editForm').on('submit', function(e){
 	e.preventDefault();
 	// show loader
 	document.getElementById('editLoad').style.display = "block";
-	// make json for changed fields
+	// serialize changed fields
 	var data ="?&";
 	$('.changed').each(function(){
 		data = data + $(this).attr("name") + "=" + $(this).val() + "&";
@@ -64,4 +64,32 @@ $('input, textarea').on('change', function(){
 /* open bid history modal */
 $('#openHistory').on('click', function(){
 	$('#historyModal').modal('show');
+});
+/* switch between auto-bid and regular bid modals */
+$('#autoBid').on('click', function(){
+	$('#bidClose').click();
+	$('#autoBidModal').modal('show');
+});
+$('#regBid').on('click', function(){
+	$('#autoBidClose').click();
+	$('#bidModal').modal('show');
+});
+
+/* pass new auto bid to servlet to process */
+$('#autoBidForm').on('submit', function(e){
+	e.preventDefault();
+	// show loader 
+	document.getElementById('autoLoad').style.display = "block";
+	$.ajax({
+		url: "/BuyMe/AuctionManagementServlet",
+		method: "POST",
+		data: $(this).serialize() + "&action=ab",
+		
+		success: function(){
+			document.getElementById('autoLoad').style.display = "none";
+			$('#autoBidClose').click();
+			alert('Auto Bidding Successfully Configured!');
+			window.location.reload();
+		}
+	})
 });
